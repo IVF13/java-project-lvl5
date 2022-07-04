9344eaf38eada6b7e1e539446a51502a690e0195
i.gordienko <i.gordienko@ylab.io>
2022-07-04 19:01:41 +0300

    Add UserDTO

---
+++ src/main/java/hexlet/code/app/model/UserDTO.java
@@ -0,0 +1,25 @@
+package hexlet.code.app.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private Date createdAt;
+
+    public UserDTO(Long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+}

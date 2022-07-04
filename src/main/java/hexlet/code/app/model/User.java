a6c37b3b97ca73893b798d3abf17ee2937e7e9e6
i.gordienko <i.gordienko@ylab.io>
2022-07-04 18:27:57 +0300

    Add User entity

---
+++ src/main/java/hexlet/code/app/model/User.java
@@ -0,0 +1,25 @@
+package hexlet.code.app.model;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    @JsonIgnore
+    private String password;
+    @CreatedDate
+    private Date createdAt;
+}

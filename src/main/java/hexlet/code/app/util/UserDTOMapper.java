e1d8b273c97f6e7eee47a2ae04577b376f8199fb
Ivan <i.gordienko@ylab.io>
2022-07-05 17:57:20 +0300

    Update UserDTOMapper

--- src/main/java/hexlet/code/app/util/UserDTOMapper.java
+++ src/main/java/hexlet/code/app/util/UserDTOMapper.java
@@ -2,13 +2,14 @@ package hexlet.code.app.util;
 
 import hexlet.code.app.model.User;
 import hexlet.code.app.model.UserDTO;
+import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.MappingTarget;
+import org.springframework.beans.factory.annotation.Autowired;
 
-@Mapper
+@Mapper(imports = User.class, componentModel = "spring")
 public interface UserDTOMapper {
 
     UserDTO userToUserDTO(User user);
-
-    User userDTOToUser(UserDTO userDTO);
 
 }

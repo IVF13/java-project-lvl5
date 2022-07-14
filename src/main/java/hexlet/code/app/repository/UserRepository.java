186d847c572fae8b2ae6b421bb6f6e36ff1b7b1d
Ivan <i.gordienko@ylab.io>
2022-07-14 22:28:19 +0300

    Add findDetailsByEmail method

--- src/main/java/hexlet/code/app/repository/UserRepository.java
+++ src/main/java/hexlet/code/app/repository/UserRepository.java
@@ -2,6 +2,7 @@ package hexlet.code.app.repository;
 
 import hexlet.code.app.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -9,6 +10,7 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
+    Optional<UserDetails> findDetailsByEmail(String email);
     Optional<User> findByEmail(String email);
 
 }

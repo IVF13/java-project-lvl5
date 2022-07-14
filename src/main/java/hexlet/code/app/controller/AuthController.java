18a386bdf48fe5ed1457adaa6700b30806aaacb7
Ivan <i.gordienko@ylab.io>
2022-07-14 22:26:06 +0300

    Add AuthController

---
+++ src/main/java/hexlet/code/app/controller/AuthController.java
@@ -0,0 +1,55 @@
+package hexlet.code.app.controller;
+
+import hexlet.code.app.model.AuthRequest;
+import hexlet.code.app.model.User;
+import hexlet.code.app.util.JwtTokenUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.validation.Valid;
+
+@RestController
+@RequestMapping("${base-url}")
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class AuthController {
+
+    private final AuthenticationManager authenticationManager;
+    private final JwtTokenUtil jwtTokenUtil;
+
+    @PostMapping("/login")
+    public ResponseEntity<String> login(@RequestBody @Valid AuthRequest request) {
+
+        System.out.println(request.password());
+        System.out.println(request.email());
+
+        try {
+            Authentication authenticate = authenticationManager
+                    .authenticate(
+                            new UsernamePasswordAuthenticationToken(
+                                    request.email(), request.password()
+                            )
+                    );
+
+            System.out.println(authenticate);
+
+            User user = (User) authenticate.getPrincipal();
+
+            System.out.println(user);
+
+            return ResponseEntity.ok().body(jwtTokenUtil.generateToken(user));
+        } catch (BadCredentialsException ex) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+        }
+    }
+
+}

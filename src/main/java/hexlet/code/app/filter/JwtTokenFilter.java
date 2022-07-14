0ea5721f2e412fcce3f66898b7b0bd806a17f364
Ivan <i.gordienko@ylab.io>
2022-07-14 22:27:18 +0300

    Add JwtTokenFilter

---
+++ src/main/java/hexlet/code/app/filter/JwtTokenFilter.java
@@ -0,0 +1,73 @@
+package hexlet.code.app.filter;
+
+import hexlet.code.app.repository.UserRepository;
+import hexlet.code.app.util.JwtTokenUtil;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+
+import static org.springframework.util.ObjectUtils.isEmpty;
+
+@Component
+@RequiredArgsConstructor(onConstructor_ = @Autowired)
+public class JwtTokenFilter extends OncePerRequestFilter {
+
+    private final JwtTokenUtil jwtTokenUtil;
+    private final UserRepository userRepository;
+
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain chain)
+            throws ServletException, IOException {
+        // Get authorization header and validate
+        final String header = request.getHeader(HttpHeaders.AUTHORIZATION);
+        if (isEmpty(header) || !header.startsWith("Bearer ")) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        // Get jwt token
+        final String token = header.split(" ")[1].trim();
+
+        // Get user identity and set it on the spring security context
+        UserDetails userDetails = userRepository
+                .findDetailsByEmail(jwtTokenUtil.getUsername(token))
+                .orElse(null);
+
+        // Validate jwt
+        if (!jwtTokenUtil.validate(token, userDetails)) {
+            chain.doFilter(request, response);
+            return;
+        }
+
+        UsernamePasswordAuthenticationToken
+                authentication = new UsernamePasswordAuthenticationToken(
+                userDetails, null,
+                userDetails == null ?
+                        List.of() : userDetails.getAuthorities()
+        );
+
+        authentication.setDetails(
+                new WebAuthenticationDetailsSource().buildDetails(request)
+        );
+
+        SecurityContextHolder.getContext().setAuthentication(authentication);
+        chain.doFilter(request, response);
+    }
+
+}

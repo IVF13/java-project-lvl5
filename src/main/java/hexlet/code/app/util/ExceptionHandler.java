7d9d8791039bad79c83b740bca59f19dc8ff6e90
i.gordienko <i.gordienko@ylab.io>
2022-07-21 16:44:20 +0300

    Add NoPermissionException handling

--- src/main/java/hexlet/code/app/util/ExceptionHandler.java
+++ src/main/java/hexlet/code/app/util/ExceptionHandler.java
@@ -6,20 +6,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.bind.annotation.ResponseStatus;
+
+import javax.naming.NoPermissionException;
 
 @ResponseBody
 @ControllerAdvice
 public class ExceptionHandler {
 
     @org.springframework.web.bind.annotation.ExceptionHandler(MethodArgumentNotValidException.class)
-    public ResponseEntity<String> handleMethodArgumentNotValidExceptions(MethodArgumentNotValidException e) {
+    public ResponseEntity<String> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.UNPROCESSABLE_ENTITY);
     }
 
     @org.springframework.web.bind.annotation.ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<String> handleNotFoundExceptions(NotFoundException e) {
+    public ResponseEntity<String> handleNotFoundException(NotFoundException e) {
         return new ResponseEntity<>(e.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @org.springframework.web.bind.annotation.ExceptionHandler(NoPermissionException.class)
+    public ResponseEntity<String> handleNoPermissionException(NoPermissionException e) {
+        return new ResponseEntity<>(e.getMessage(), HttpStatus.METHOD_NOT_ALLOWED);
     }
 
 }

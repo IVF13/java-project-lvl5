13b6831545f06ce4b3fef3b4601f4b72848853ef
i.gordienko <i.gordienko@ylab.io>
2022-07-26 18:51:08 +0300

    Rename variable

--- src/main/java/hexlet/code/app/controller/UserController.java
+++ src/main/java/hexlet/code/app/controller/UserController.java
@@ -22,11 +22,11 @@ import static hexlet.code.app.controller.UserController.USER_CONTROLLER_PATH;
 @RequiredArgsConstructor(onConstructor_ = @Autowired)
 public class UserController {
     public static final String USER_CONTROLLER_PATH = "/users";
-    public static final String USER_ID = "/{id}";
+    public static final String USER_ID_IN_CONTROLLER = "/{id}";
 
     private final UserService userService;
 
-    @GetMapping(path = USER_ID)
+    @GetMapping(path = USER_ID_IN_CONTROLLER)
     public ResponseEntity<UserDTO> getUserById(@PathVariable String id) throws NoPermissionException {
         UserDTO userDTO = userService.getUserById(id);
         return ResponseEntity.ok().body(userDTO);
@@ -43,14 +43,14 @@ public class UserController {
         return ResponseEntity.status(HttpStatus.CREATED).body(userService.createUser(user));
     }
 
-    @PutMapping(path = USER_ID)
+    @PutMapping(path = USER_ID_IN_CONTROLLER)
     public ResponseEntity<UserDTO> updateUser(@PathVariable String id, @RequestBody @Valid User user)
             throws NoPermissionException {
         UserDTO userDTO = userService.updateUser(id, user);
         return ResponseEntity.ok().body(userDTO);
     }
 
-    @DeleteMapping(path = USER_ID)
+    @DeleteMapping(path = USER_ID_IN_CONTROLLER)
     public String deleteUser(@PathVariable String id) throws NoPermissionException {
         return userService.deleteUser(id);
     }

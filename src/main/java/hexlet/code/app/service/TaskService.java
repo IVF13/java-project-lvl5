b183939a41f421ab4af2ba511bfe402e13f7238d
i.gordienko <i.gordienko@ylab.io>
2022-07-27 18:41:05 +0300

    Add TaskService

---
+++ src/main/java/hexlet/code/app/service/TaskService.java
@@ -0,0 +1,19 @@
+package hexlet.code.app.service;
+
+import hexlet.code.app.model.Task;
+
+import java.util.List;
+
+public interface TaskService {
+
+    Task getTaskById(String id);
+
+    List<Task> getAllTasks();
+
+    Task createTask(Task task);
+
+    Task updateTask(String id, Task task);
+
+    String deleteTask(String id);
+
+}

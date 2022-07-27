7b95f3264604004ed8de5756755e4ff84ea6a9c2
i.gordienko <i.gordienko@ylab.io>
2022-07-27 18:45:24 +0300

    Change type

--- src/main/java/hexlet/code/app/repository/TaskRepository.java
+++ src/main/java/hexlet/code/app/repository/TaskRepository.java
@@ -1,11 +1,11 @@
 package hexlet.code.app.repository;
 
-import hexlet.code.app.model.TaskStatus;
+import hexlet.code.app.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface TaskRepository extends JpaRepository<TaskStatus, Long> {
-    Optional<TaskStatus> findByName(String name);
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    Optional<Task> findByName(String name);
 
 }

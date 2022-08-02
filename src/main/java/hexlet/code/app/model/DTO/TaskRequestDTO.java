8e7819b4cbbb3977e8a44d6be01fba353470f1a6
Ivan <i.gordienko@ylab.io>
2022-08-02 16:48:21 +0300

    Add option adding labels list when creating tasks

--- src/main/java/hexlet/code/app/model/DTO/TaskRequestDTO.java
+++ src/main/java/hexlet/code/app/model/DTO/TaskRequestDTO.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -19,7 +20,16 @@ public class TaskRequestDTO {
 
     private Long executorId;
 
+    private List<Long> labelIds;
+
     @NotNull
     private Long taskStatusId;
+
+    public TaskRequestDTO(String name, String description, Long executorId, Long taskStatusId) {
+        this.name = name;
+        this.description = description;
+        this.executorId = executorId;
+        this.taskStatusId = taskStatusId;
+    }
 
 }
--- src/main/java/hexlet/code/app/model/DTO/TaskResponseDTO.java
+++ src/main/java/hexlet/code/app/model/DTO/TaskResponseDTO.java
@@ -1,11 +1,13 @@
 package hexlet.code.app.model.DTO;
 
+import hexlet.code.app.model.entity.Label;
 import hexlet.code.app.model.entity.TaskStatus;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.Date;
+import java.util.List;
 
 @Data
 @AllArgsConstructor
@@ -15,6 +17,7 @@ public class TaskResponseDTO {
     private UserDTO author;
     private UserDTO executor;
     private TaskStatus taskStatus;
+    private List<Label> labels;
     private String name;
     private String description;
     private Date createdAt;

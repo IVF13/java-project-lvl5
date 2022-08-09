d40ad420d56e425c2d8d0db6f05be658e2ee5a06
Ivan <i.gordienko@ylab.io>
2022-08-09 17:37:23 +0300

    Remove findDetailsByEmail

--- src/main/java/hexlet/code/repository/UserRepository.java
+++ src/main/java/hexlet/code/repository/UserRepository.java
@@ -2,7 +2,6 @@ package hexlet.code.repository;
 
 import hexlet.code.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,7 +9,6 @@ import java.util.Optional;
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    Optional<UserDetails> findDetailsByEmail(String email);
     Optional<User> findByEmail(String email);
 
 }
